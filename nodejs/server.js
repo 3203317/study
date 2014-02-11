9399cbac2f3c5cda56c1e73e42a1c48be210665b
3203317 <3203317@qq.com>
2014-02-11 13:15:17 +0800

    haha

---
+++ nodejs/server.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url")
+
+function onStart($port) {
+
+	http.createServer(function ($request, $response) {
+
+		console.log(url)
+
+		$response.writeHead(200, {"Content-Type": "text/plain"})
+		$response.write("Hello, World!")
+		$response.end()
+
+	}).listen($port);
+
+	console.log("Server running at http://127.0.0.1:"+ $port +"/");
+}
+
+exports.start = onStart;
