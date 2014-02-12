81132d159624202a2c1fe607afe3e98cd02bda52
3203317 <3203317@qq.com>
2014-02-12 17:50:10 +0800

    haha

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,7 @@
 var http = require("http"),
 	fs = require("fs"),
 	url = require("url"),
+	path = require("path"),
 	conf = require("./server.conf"),
 	router = require("./router.js"),
 	mime = require("./mime.js").types;
@@ -10,13 +11,26 @@ function onStart() {
 		port = appConf.port,
 		welcome = appConf.welcome;
 
-	console.log(appConf);
+	// console.log(appConf);
 
 	var server = http.createServer(function ($request, $response) {
 		var realPath = router.route($request);
-		console.log(realPath)
 
-		if ("/" == realPath) { realPath = "/"+ welcome };
+		/* 获取文件后缀 */
+		var ext = path.extname(realPath);
+		ext = ext ? ext.slice(1) : "unknown";
+
+		/* */
+		var contentType = mime[ext] || "text/plain";
+
+
+		var eee = {
+			fileMatch: /^(gif|png|jpg|js|css|html)$/ig,
+			maxAge: 60 * 60 * 24 * 365
+		};
+
+
+		// if ("/" == realPath) { realPath = "/"+ welcome };
 
 		realPath = "."+ realPath;
 
@@ -28,16 +42,35 @@ function onStart() {
                 $response.end();
 			} else {
 				if($stats.isDirectory()){
-
+					console.log("isDirectory")
+					$response.end();
 				} else {
-					$response.writeHead(200, { "Content-Type": "text/html" });
-					var rst = fs.createReadStream(realPath, { encoding: "utf-8" });
-					rst.on("error", function ($err){
-						console.log($err)
-					});
+					$response.setHeader("Content-Type", contentType);
 
-					// console.log(rst)
-					rst.pipe($response);
+					var lastModified = $stats.mtime.toUTCString();
+					var ifModifiedSince = "If-Modified-Since".toLowerCase();
+					$response.setHeader("Last-Modified", lastModified);
+
+					if (ext.match(eee.fileMatch)) {
+						var expires = new Date();
+						expires.setTime(expires.getTime() + eee.maxAge * 1000);
+						$response.setHeader("Expires", expires.toUTCString());
+						$response.setHeader("Cache-Control", "max-age=" + eee.maxAge);
+					}
+
+					if($request.headers[ifModifiedSince] && lastModified == $request.headers[ifModifiedSince]) {
+						console.log("从浏览器cache里取")
+						$response.writeHead(304, "Not Modified");
+						$response.end();
+					}else{
+						var rst = fs.createReadStream(realPath, { encoding: "utf-8" });
+						rst.on("error", function ($err){
+							console.log($err)
+						});
+
+						// console.log(rst)
+						rst.pipe($response);
+					}
 				}
 			}
 		});
