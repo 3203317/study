853fe53f416cab0ae35c6d21da41fe694a6065d6
3203317 <3203317@qq.com>
2014-03-01 21:13:11 +0800

    haha

---
+++ myblog/public/index/index.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function(){
+	console.log("Hello, World!");
+
+	$(".flexslider").flexslider({
+		animation: "slide"
+	});
+
+	$("#qrcode").pin({
+		minWidth: 940,
+		top: 60
+	})
+	
+	$("#loadMore").click(function(){
+        var that = this;
+        var $that = $(that);
+        var text = $that.text();
+        $that.text("努力加载中...");
+        var currentPage = $that.data("current-page") + 1;
+        var data = { data: JSON.stringify({ Current: currentPage }) };
+        
+        $.ajax({
+			url: $().olxUtilRandomUrl($that.data("url")),
+			type: "GET",
+			dataType: "html",
+			data: data
+		}).done(function(responseText) {
+		    if("" != $.trim(responseText)){		    
+                $that.data("current-page", currentPage);		    
+			    $("#articleIntros").append(responseText);
+		    }			
+		}).complete(function(){
+            $that.text(text);
+		});
+	});
+});
