09d89ed00d62bc239715edbc94c44526062bec2c
3203317 <3203317@qq.com>
2014-03-13 14:43:00 +0800

    haha

--- myblog/controllers/user.js
+++ myblog/controllers/user.js
@@ -5,17 +5,11 @@ var virtualPath = '';
 var title = 'FOREWORLD 洪荒';
 
 exports.login = function(req, res, next) {
-	Category.findCategorys(function(err, docs){
-		if(err){
-			console.log(err);
-		}
-		res.render('User/Login', { 
-			title: title,
-			atitle: '登陆',
-			description: '个人博客',
-			keywords: ',登陆,Bootstrap3',
-			virtualPath: virtualPath +'/',
-			categorys: docs
-		});
+	res.render('User/Login', { 
+		title: title,
+		atitle: '登陆',
+		description: '个人博客',
+		keywords: ',登陆,Bootstrap3',
+		virtualPath: virtualPath +'/'
 	});
 };
